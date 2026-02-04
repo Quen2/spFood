1d2d39a7d473eb80172423fc28e2f6b24a66d90c
QuentinG <ZexFrost1@gmail.com>
2026-02-04 11:41:57 +0100

    [+] First push, base structure

---
+++ src/main/java/com/spfood/spfood/SpfoodApplication.java
@@ -0,0 +1,13 @@
+package com.spfood.spfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spfood/spfood/SpfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spfood.spfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
