cbaaed04a655eae6070826c226b32bac6a4ef7b3
QuentinG <ZexFrost1@gmail.com>
2026-03-05 12:00:42 +0100

    Fix: added email auth

--- src/main/java/com/cda/spfood/services/UserService.java
+++ src/main/java/com/cda/spfood/services/UserService.java
@@ -6,15 +6,34 @@ import com.cda.spfood.entities.User;
 import com.cda.spfood.mappers.UserMapper;
 import com.cda.spfood.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.get().getEmail())
+                .password(user.get().getPassword())
+                .roles(user.get().getRole())
+                .build();
+    }
 
     public List<UserResponseDTO> findAll() {
         List<User> users = userRepository.findAll();
@@ -30,14 +49,24 @@ public class UserService {
     }
 
     public void update(Integer id, UserRequestDTO userRequestDTO) {
-        userRepository.save(userMapper.toEntity(userRequestDTO));
+        User existingUser = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        User updatedUser = userMapper.toEntity(userRequestDTO);
+        updatedUser.setId(id);
+        updatedUser.setPassword(passwordEncoder.encode(updatedUser.getPassword()));
+        userRepository.save(updatedUser);
     }
 
     public void save(UserRequestDTO userRequestDTO) {
-        userRepository.save(userMapper.toEntity(userRequestDTO));
+        User user = userMapper.toEntity(userRequestDTO);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
     }
 
     public void delete(Integer id) {
         userRepository.deleteById(id);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 }
